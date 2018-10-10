55494335b4839d0fab54516f6bcf6b5b033e53dd
Alexander Richards <a.richards@imperial.ac.uk>
2018-10-10 11:09:35 +0100

    new stuff

--- solid/ParametricJobs.py
+++ solid/ParametricJobs.py
@@ -1,16 +1,20 @@
 from sqlalchemy import Integer, TEXT
-from productionsystem.sql.SQLTableBase import SmartColumn as Column
+from productionsystem.sql.SQLTableBase import SmartColumn
 from productionsystem.sql.models.ParametricJobs import ParametricJobs
 
 class SolidParametricJobs(ParametricJobs):
     __mapper_args__ = {'polymorphic_identity': 'solidparametricjobs'}
-    solidsim_version = Column(TEXT, allowed=True)
-    solidsim_macro = Column(TEXT, allowed=True)
-    solidsim_nevents = Column(Integer, allowed=True)
-    solidsim_inputfiletype = Column(TEXT, allowed=True)
-    solidsim_output_lfn = Column(TEXT, allowed=True)
-    saffron2_ro_version = Column(TEXT, allowed=True)
-    saffron2_output_lfn = Column(TEXT, allowed=True)
-    seed = Column(Integer, allowed=True)
-    jobnumber_start = Column(Integer, allowed=True)
-    day = Column(TEXT, allowed=True)
+    solidsim_version = SmartColumn(TEXT, allowed=True)
+    solidsim_macro = SmartColumn(TEXT, allowed=True)
+    solidsim_nevents = SmartColumn(Integer, allowed=True)
+    solidsim_inputfiletype = SmartColumn(TEXT, allowed=True)
+    solidsim_output_lfn = SmartColumn(TEXT, allowed=True)
+    saffron2_ro_version = SmartColumn(TEXT, allowed=True)
+    saffron2_output_lfn = SmartColumn(TEXT, allowed=True)
+    seed = SmartColumn(Integer, allowed=True)
+    jobnumber_start = SmartColumn(Integer, allowed=True)
+    day = SmartColumn(TEXT, allowed=True)
+
+
+    def _setup_dirac_job(self, job, tmp_runscript):
+        pass
